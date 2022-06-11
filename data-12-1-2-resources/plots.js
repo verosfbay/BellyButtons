016bbfd01ebd078fbf22a29afe3fa1568db06300
Veronica Lobkina <you@example.com>
2022-06-10 19:56:40 -0700

    working

--- data-12-1-2-resources/plots.js
+++ data-12-1-2-resources/plots.js
@@ -46,7 +46,7 @@ var trace = {
    */ 
 
    // Scatter plot
-   
+   /*
    var trace = {
     x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
     "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
@@ -59,3 +59,78 @@ var trace = {
     title: "Scatter Plot",
    };
    Plotly.newPlot("plotArea", data, layout);
+*/
+
+/* Console log 
+var numbers = [0,2,4,6,8];
+var doubled = numbers.map(function(num){
+    return num + 5;
+});
+console.log(doubled);
+*/
+
+/*
+var cities = [
+    {
+      "Rank": 1,
+      "City": "San Antonio ",
+      "State": "Texas",
+      "Increase_from_2016": "24208",
+      "population": "1511946"
+    },
+    {
+      "Rank": 2,
+      "City": "Phoenix ",
+      "State": "Arizona",
+      "Increase_from_2016": "24036",
+      "population": "1626078"
+    },
+    {
+      "Rank": 3,
+      "City": "Dallas",
+      "State": "Texas",
+      "Increase_from_2016": "18935",
+      "population": "1341075"
+    }
+];
+
+var cityNames = cities.map(function(city){
+    return city.City;
+});
+console.log(cityNames);
+*/
+
+/*
+var familyAge = [2,3,39,37,9];
+ 
+var olderThanFive = familyAge.filter(function(age){
+    return age > 5;
+});
+*/ 
+
+/*
+var numbers = [1,2,3,4,5];
+
+
+var doubled = numbers.map(num => num * 2);
+console.log(doubled);
+*/
+
+/*
+var familyAge = [3,2,39,37,9];
+var sortedAge = familyAge.sort((a,b) => a - b);
+console.log(sortedAge);
+*/ 
+
+/*
+var integers = [0,1,2,3,4,5];
+var slice1 = integers.slice(0,2);
+
+the slice()method begins selecting the array at index position 0, 
+and stops right before reaching index position 2.
+
+To slice to the end of an array, you can omit the second argument:
+var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
+words.slice(3, );
+*/ 
+
